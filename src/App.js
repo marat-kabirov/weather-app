e39582ec9e8cfcfd8405a3e171d3b92ef203a534
marat-kabirov <kabirovmarat913@gmail.com>
2026-03-25 20:52:52 +0100

    Add project README

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
-
+import {useState} from 'react';
+ 
 function App() {
+  const [city, setCity] = useState('');
+  const [weather, setWeather] = useState(null);
+
+  const handleSearch = async () => {
+    const API_KEY = process.env.REACT_APP_API_KEY;
+    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`);
+    const data = await response.json();
+    setWeather(data);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Weather App</h1>
+      <input 
+      type ="text" 
+      placeholder ="Enter city name" 
+      value={city}
+      onChange={(e) => setCity(e.target.value)} />
+      <button onClick = {handleSearch}>Get Weather</button>
+      {weather && (
+        <div>
+          <h2>{weather.name}</h2>
+          <p>Temperature: {weather.main.temp} °C</p>
+          <p>Weather: {weather.weather[0].description}</p>
+          <p>Humidity: {weather.main.humidity}%</p>
+        </div>
+      )}
     </div>
   );
 }
